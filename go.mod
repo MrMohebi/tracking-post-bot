5fa6d9eba2ba70944ae2f861612865744da4ce5c
MMMohebi <MMMohebi@outlook.com>
2023-02-28 13:48:04 +0330

    push

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module MrMohebi/tracking-post-telegram-bot
 
 go 1.20
 
-require gopkg.in/telebot.v3 v3.1.2 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	gopkg.in/telebot.v3 v3.1.2
+)
+
+require (
+	github.com/anaskhan96/soup v1.2.5 // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"MrMohebi/tracking-post-telegram-bot/requests"
+	"github.com/anaskhan96/soup"
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v3"
 	"log"
 	"os"
 	"time"
-
-	tele "gopkg.in/telebot.v3"
 )
 
 func main() {
+	println("aaaaa")
+
+	err := godotenv.Load()
+
 	pref := tele.Settings{
 		Token:  os.Getenv("TEL_TOKEN"),
 		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
@@ -19,6 +25,24 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+
+	b.Handle(tele.OnText, func(c tele.Context) error {
+		var (
+		//user = c.Sender()
+		//text = c.Text()
+		)
+		err := c.Send("wait a second...")
+		if err != nil {
+			return err
+		}
+		bodyString, err := requests.PostSendRequest("104530134300045270076178")
+		if err != nil {
+
+		}
+		doc := soup.HTMLParse(string(bodyString))
+		links := doc.Find("div", "id", "txtVoteR").FindAll("div")
+		return c.Send(links)
+	})
 
 	b.Handle("/hello", func(c tele.Context) error {
 		return c.Send("Hello!")
